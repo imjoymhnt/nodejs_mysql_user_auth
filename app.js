f9c8f91480e31fe42627e9d825e5375a7fdb724e
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2022-06-04 12:20:53 +0530

    cors added

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createPool({
